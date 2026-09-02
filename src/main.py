f74d61fe168e577da3f507da7aac67c5b85257cc
Tati <tamita@Mac.lan>
2026-09-01 22:12:59 -0500

    Se crea el metodo para generar la informacion de los conteos, total price, minimo y maximo price, dicha informacion es por el archivo que se esta procesando

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from src.config.settings import (
 from src.extract.data_extractor import read_raw_csv
 from src.load.data_loader import load_to_postgres
 from src.load.db_bootstrap import ensure_database_exists
-from src.transform.data_transformer import transform_dataframe
+from src.transform.data_transformer import get_stats_info, transform_dataframe
 from src.utils.logging_config import setup_logging
 
 logger = logging.getLogger(__name__)
@@ -21,10 +21,11 @@ def main() -> None:
     file_path = RAW_FILE_1
     df_raw = read_raw_csv(file_path)
     df_transformed = transform_dataframe(df_raw, source_file_name=file_path.name)
+    df_statistics = get_stats_info(df_transformed)
     load_to_postgres(df_transformed)
 
-    print(df_transformed)
-
+    # print(df_transformed)
+    print(df_statistics)
     logger.info("Pipeline se ejecutó exitosamente")
 
 
